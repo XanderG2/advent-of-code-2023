588d7c422ce52061b3ddd546ea8968f3977287de
XanderG2 <XanderG2@users.noreply.github.com>
2023-12-16 14:55:45 +0000

    day 4 part 2

--- day4.py
+++ day4.py
@@ -1,25 +1,47 @@
 with open("inputs/day4.txt", "r") as f:
-    input = f.read()
+    input = f.read().strip()
 
 lines = input.splitlines()
 
-totalsum = 0
+def partOne():
+    totalsum = 0
 
-for line in lines:
-    id = int(line.split()[1].split(":")[0])
-    winning = line.split("|")[0].split(": ")[1].split()
-    numbers = line.split("| ")[1].split()
-    wins = 0
-    for num in numbers:
-        if num in winning:
-            wins += 1
-    print(f"Card {id}: {wins}")
-    total = 0
-    for x in range(wins):
-        if x == 0:
-            total = 1
-        else:
-            total *= 2
-    totalsum += total
+    for line in lines:
+        id = int(line.split()[1].split(":")[0])
+        winning = line.split("|")[0].split(": ")[1].split()
+        numbers = line.split("| ")[1].split()
+        wins = 0
+        for num in numbers:
+            if num in winning:
+                wins += 1
+        total = 0
+        for x in range(wins):
+            if x == 0:
+                total = 1
+            else:
+                total *= 2
+        totalsum += total
 
-print(totalsum)
+    print(totalsum)
+
+def partTwo():
+    dupes = [1]*len(lines)
+    totalScratchcards = 0
+    for line in lines:
+        id = int(line.split()[1].split(":")[0])
+        winning = line.split("|")[0].split(": ")[1].split()
+        numbers = line.split("| ")[1].split()
+        wins = 0
+        for num in numbers:
+            if num in winning:
+                wins += 1
+        for x in range(id, id+wins):
+            try: dupes[x] += 1 * dupes[id-1]
+            except: 
+                print(f"failed. {x}")
+    for x in dupes:
+        totalScratchcards += x
+    print(totalScratchcards)
+
+partOne()
+partTwo()
